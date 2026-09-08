b8d2e0b2291bb3d17846ce17a5ce4ce2eb824ca9
Adedeji Olawale <adedejiolawale@Adedejis-MacBook-Pro.local>
2026-09-08 13:03:31 -0400

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,23 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";
+// import * as github from "@actions/github";
 import * as exec from "@actions/exec";
 
 function run() {
+  // 1. get some input values
+  const bucket = core.getInput("bucket", {
+    required: true,
+  });
+  const bucketRegion = core.getInput("bucket-region", {
+    required: false,
+  });
+  const distFolder = core.getInput("dist-folder", {
+    required: false,
+  });
+
+  // 2. Upload Files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice("Hello from my custom Javascript Action!");
 }
 
