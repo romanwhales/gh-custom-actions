5852f48f70b0f1119a32046aa262de933085f123
Adedeji Olawale <adedejiolawale@Adedejis-MacBook-Pro.local>
2026-09-08 16:24:36 -0400

    checking for uploaded directories

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -19,7 +19,7 @@ function run() {
   console.log(`s3 uri ${s3Uri}`);
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`;
   core.setOutput("website-url", websiteUrl);
 }
 
